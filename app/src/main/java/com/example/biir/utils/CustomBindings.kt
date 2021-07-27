249983b1dfcb4ddafbd4f9cebb2961ed5120bf15
Vinu Polly <vinupolly@gmail.com>
2021-07-27 17:50:30 +0100

    Added IBU logic to custom adapter, Done

--- app/src/main/java/com/example/biir/utils/CustomBindings.kt
+++ app/src/main/java/com/example/biir/utils/CustomBindings.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.example.biir.R
+import java.lang.Exception
 
 @BindingAdapter("loadImage")
 fun bindingImage(view: ImageView?, url: String?) {
@@ -22,6 +24,30 @@ fun setFloat(view: TextView, value: Float) {
     } else {
         val stringValue: String = value.toInt().toString()
         view.text = stringValue
+    }
+}
+
+@BindingAdapter("displayIbuText")
+fun setText(view: TextView, value: String?) {
+    if (value == null || value.trim() == "null") {
+        view.text = ""
+    } else {
+        try {
+            val ibu = Integer.parseInt(value)
+            when {
+                ibu <= 20 -> {
+                    view.text = view.context.getString(R.string.smooth)
+                }
+                ibu in 21..50 -> {
+                    view.text = view.context.getString(R.string.bitter)
+                }
+                else -> {
+                    view.text = view.context.getString(R.string.hipster_plus)
+                }
+            }
+        } catch (e:Exception) {
+            view.text = value
+        }
     }
 }
 
